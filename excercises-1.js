0a0b4888342215b578859ca5046867d459d768e1
Beovonni Glover <47301902+Innovoeb@users.noreply.github.com>
2019-03-19 17:36:30 -0700

    exercises-1.js initial commit

--- excercises-1.js
+++ excercises-1.js
@@ -20,7 +20,7 @@
 ///////////////////////////////////////////////////////
 
 // READ FIRST
-// The assertions are there only to help you make sure 
+// The assertions are there only to help you make sure
 // your functions are written correctly. There is no need
 // to change them. If you write your function correctly,
 // the assertions will do nothing. If you write your function
@@ -32,6 +32,8 @@
 // Write a function called `sum` that takes two
 // parameters and returns the sum of those 2 numbers.
 
+var sum = (a, b) => a + b;
+
 console.assert(sum(2,4) === 6)
 console.assert(sum(45, 23) === 68)
 console.assert(sum(-2, -57) === -59)
@@ -40,6 +42,8 @@ console.assert(sum(-2, -57) === -59)
 // Write a function named `avg` that takes 3 parameters
 // and returns the average of those 3 numbers.
 
+var avg = (a, b, c) => (a + b + c) / 3;
+
 console.assert(avg(2,3,4) === 3)
 console.assert(avg(234, 63, 234) === 177)
 console.assert(avg(123,234,345) === 234)
@@ -47,6 +51,8 @@ console.assert(avg(123,234,345) === 234)
 // Q3
 // Write a function called `getLength` that takes one
 // parameter (a string) and returns the length
+
+var getLength = (str) => str.length;
 
 console.assert(getLength('Michael') === 7)
 console.assert(getLength('Javascript') === 10)
@@ -58,6 +64,9 @@ console.assert(getLength('supercalifragilisticexpialidocious') === 34)
 // parameter is greater than the second. Otherwise
 // the function should return `false`.
 
+var greaterThan = (a, b) => a > b
+
+
 console.assert(greaterThan(2, 1) === true)
 console.assert(greaterThan(23, 23) === false)
 console.assert(greaterThan(-234, 1) === false)
@@ -68,13 +77,15 @@ console.assert(greaterThan(-234, 1) === false)
 // is formated like "Hello, Name!" where *Name*
 // is the parameter that was passed in.
 
+var greet = (name) => ("Hello, " + name + "!");
+
 console.assert(greet("Michael") === "Hello, Michael!")
 console.assert(greet("R2D2") === "Hello, R2D2!")
 console.assert(greet("Iron Man") === "Hello, Iron Man!")
 
 // Q6
 // Write a function called `madlib` that takes
-// 4 or more parameters (words). The function 
+// 4 or more parameters (words). The function
 // should insert the words into a pre-defined
 // sentence. Finally the function should return
 // that sentence.
@@ -82,6 +93,8 @@ console.assert(greet("Iron Man") === "Hello, Iron Man!")
 // strings. For example:
 // words: "quick", "fox", "fence"
 // sentence: "quick brown fox jumps over the fence"
+
+var madlib = (a, b, c, d,) => ("The " + a + " brown " + b + " " + c + " over the " + d + ".");
 
 console.assert(madlib("quick","fox","jumps","fence")=== "The quick brown fox jumps over the fence.")
 console.assert(madlib("fat","rabbit","hops","wall")=== "The fat brown rabbit hops over the wall.")
